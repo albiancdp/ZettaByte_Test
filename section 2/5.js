491fd8ae6c8581b8ad3f32d6b71551128b367f11
albiancdp <albiancdp@gmail.com>
2022-06-25 22:32:51 +0700

    fix: section 2 number 5

--- section 2/5.js
+++ section 2/5.js
@@ -26,6 +26,26 @@ const groups = 3;
 
 function result(students, groups) {
     // your code here
+    let result = [];
+    let newArrayStudent = new Array(...students);
+    const lengthStudents = newArrayStudent.length;
+    const lengthGroup = Math.ceil(lengthStudents / groups);   // get total student per group
+    // sort by first name
+    newArrayStudent.sort((a, b) => {
+        if (a.firstName < b.firstName) {
+            return -1;
+        } else if (a.firstName > b.firstName) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+    // slice per group
+    for (let i = 0; i < groups; i++) {
+        console.log(i * lengthGroup, (i + 1) * lengthGroup);
+        result.push(newArrayStudent.slice(i * lengthGroup, (i + 1) * lengthGroup));
+    }
+    return result;
 }
 
 console.log(result(students, groups));
